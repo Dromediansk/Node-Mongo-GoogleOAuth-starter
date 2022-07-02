1e8ab072405cb36cac79faa35b3c723efb5d3263
Dromediansk <miroslav.pillar@senacor.com>
2022-07-02 18:39:09 +0200

    removing graphQl from repo

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,6 @@
 import Fastify from "fastify";
 import helmet from "@fastify/helmet";
 import cors from "@fastify/cors";
-import merciurius from "mercurius";
-import schema from "./graphql/schema";
 
 const app = Fastify({
   logger: true,
@@ -15,11 +13,6 @@ app.register(cors, {
 app.register(helmet, {
   contentSecurityPolicy:
     process.env.NODE_ENV === "production" ? undefined : false,
-});
-
-app.register(merciurius, {
-  schema,
-  graphiql: true,
 });
 
 export default app;
