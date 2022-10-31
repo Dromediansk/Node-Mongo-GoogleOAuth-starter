562c75623e30c17731adbf24bf077c35c1123e81
Dromediansk <miroslav.pillar@senacor.com>
2022-10-31 12:11:37 +0100

    adding tests

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,12 +1,23 @@
 import request from "supertest";
 import app from "./app";
 
-describe("GET endpoint /", () => {
-  it("should return 200 and html file", async () => {
+describe("GET /", () => {
+  it("should return response with HTML file", async () => {
     const response = await request(app).get("/");
 
     expect(response.statusCode).toEqual(200);
     expect(response.type).toBe("text/html");
     expect(response.text).toMatch("Node Mongo OAuth Google starter");
+  });
+});
+
+describe("route /api", () => {
+  it("should return 401 Unauthorized if user is not logged in", async () => {
+    const response = await request(app)
+      .get("/api/users")
+      .set("Cookie", "test123");
+
+    expect(response.statusCode).toBe(401);
+    expect(response.text).toContain("You must be logged in!");
   });
 });
